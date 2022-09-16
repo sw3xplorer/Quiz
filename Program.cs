8140a9898ae0a4b3f2367abbb197de42d49d1af5
Robert Popa <robert.popa@elev.ga.ntig.se>
2022-09-16 12:17:17 +0200

    Start of code

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿//string name = "Micke";
+
+// if (true)
+// bool isAlive = name == "Micke";
+// {
+//     Console.WriteLine("Yes");
+// }
+
+// Console.WriteLine(isAlive);
+// Console.ReadLine();
+
+Console.WriteLine(@" _    _      _                            _            
+| |  | |    | |                          | |         _ 
+| |  | | ___| | ___ ___  _ __ ___   ___  | |_ ___   (_)
+| |/\| |/ _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \     
+\  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) |  _ 
+ \/  \/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/  (_)
+                                                       
+                                                       ");
+Console.WriteLine(@"  ___           _                         _     
+  |_  |         | |                       (_)    
+    | |_   _ ___| |_    __ _    __ _ _   _ _ ____
+    | | | | / __| __|  / _` |  / _` | | | | |_  /
+/\__/ / |_| \__ \ |_  | (_| | | (_| | |_| | |/ / 
+\____/ \__,_|___/\__|  \__,_|  \__, |\__,_|_/___|
+                                  | |            
+                                  |_|            ");
+Console.WriteLine();
+Console.WriteLine(@"_____      _              _              _             _   
+|  ___|    | |            | |            | |           | |  
+| |__ _ __ | |_ ___ _ __  | |_ ___    ___| |_ __ _ _ __| |_ 
+|  __| '_ \| __/ _ \ '__| | __/ _ \  / __| __/ _` | '__| __|
+| |__| | | | ||  __/ |    | || (_) | \__ \ || (_| | |  | |_ 
+\____/_| |_|\__\___|_|     \__\___/  |___/\__\__,_|_|   \__|
+                                                            
+                                                            ");
+Console.ReadLine();
+
+Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+string nameS = name[0].ToString().ToUpper() + name[1..].ToLower();
+
+Console.WriteLine($"\nHello {nameS}.");
+
+while (true)
+{
+
+Console.WriteLine("Choose one of the following categories:");
+
+
+
+}
+Console.ReadLine();
