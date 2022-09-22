60c938ab0bff7275f7de15a8910291500ef84f10
Robert Popa <robert.popa@elev.ga.ntig.se>
2022-09-22 15:48:34 +0200

    Ver without problems hopefully

--- Program.cs
+++ Program.cs
@@ -43,15 +43,15 @@ Console.WriteLine(@"_____      _              _              _             _
 Console.ReadLine();
 string name = "";
 while (name == "")
-    {
+{
     Console.Write("\nEnter your name: ");
     name = Console.ReadLine();
-      if (name == "")
-      {
+    if (name == "")
+    {
         Console.WriteLine("\nIs it that hard to write your name?????");
         Task.Delay(1500).Wait();
-      }
     }
+}
 // Console.Write("Enter your name: ");
 // name = Console.ReadLine();
 string nameS = name[0].ToString().ToUpper() + name[1..].ToLower();
@@ -63,6 +63,7 @@ while (true)
 {
     string category = "";
     int tries = 2;
+    int points = 0;
     Console.WriteLine("Choose one of the following categories:");
     Task.Delay(1000).Wait();
     Console.WriteLine("\nA: General stuff");
@@ -74,13 +75,13 @@ while (true)
     Task.Delay(1000).Wait();
     while (category != "A" && category != "B")
     {
-    Console.Write("\nChoose a category: ");
-    category = Console.ReadLine().ToUpper();
-      if (category != "A" && category != "B")
-      {
-        Console.WriteLine("\nIs it that hard to write an actual category?");
-        Task.Delay(1500).Wait();
-      }
+        Console.Write("\nChoose a category: ");
+        category = Console.ReadLine().ToUpper();
+        if (category != "A" && category != "B")
+        {
+            Console.WriteLine("\nIs it that hard to write an actual category?");
+            Task.Delay(1500).Wait();
+        }
     }
     if (category == "A")
     {
@@ -97,69 +98,95 @@ while (true)
         category_A_answer_1 = Console.ReadLine().ToLower();
         while (category_A_answer_1 != "france")
         {
-          Console.Write($"That's the wrong answer. Tries left {tries}.");
-          tries--;
-          Console.Write("\nTry again: ");
-          category_A_answer_1 = Console.ReadLine().ToLower();
-          if (category_A_answer_1 != "france" && tries == 0)
-          {
-            Console.WriteLine("\nOut of tries. The correct answer is France.");
-            Task.Delay(1000).Wait();
-            break;
-          }
+            Console.Write($"That's the wrong answer. Tries left {tries}.");
+            tries--;
+            Console.Write("\nTry again: ");
+            category_A_answer_1 = Console.ReadLine().ToLower();
+            if (category_A_answer_1 != "france" && tries == 0)
+            {
+                Console.WriteLine("\nOut of tries. The correct answer is France.");
+                Task.Delay(1000).Wait();
+                break;
+            }
         }
-        if (category_A_answer_1 == "france")
+        if (category_A_answer_1 == "france" && tries == 2)
         {
-          Console.WriteLine("\nCorrect answer");
+            Console.WriteLine("\nCorrect answer");
+            points++;
         }
-        Console.WriteLine("\nQuestion 2:");
-        Task.Delay(1000).Wait();
-        Console.WriteLine("How many wonders of the world are there?");
-        string category_A_answer_2;
+        else if (category_A_answer_1 == "france" && tries != 2)
+        {
+            Console.WriteLine("\nCorrect answer");
+        }
 
-        Console.Write("Answer: ");
-        category_A_answer_2 = Console.ReadLine().ToLower();
-        while (category_A_answer_2 != "seven" && category_A_answer_2 != "7")
+        if (tries > -1)
         {
-          Console.Write($"That's the wrong answer. Tries left {tries}.");
-          tries--;
-          Console.Write("\nTry again: ");
-          category_A_answer_2 = Console.ReadLine().ToLower();
-          if (category_A_answer_2 != "seven" && category_A_answer_2 != "7" && tries == 0)
-          {
-            Console.WriteLine("\nOut of tries. The correct answer is seven (7).");
-            Task.Delay(1000).Wait();
-            break;
-          }
-        }
-        if (category_A_answer_2 == "seven" || category_A_answer_2 == "7")
-        {
-          Console.WriteLine("\nCorrect answer");
-        }
-        
-        Console.WriteLine("\nQuestion 3:");
-        Task.Delay(1000).Wait();
-        Console.WriteLine("What is a group of crows called?");
-        string category_A_answer_3;
 
-        Console.Write("Answer: ");
-        category_A_answer_3 = Console.ReadLine().ToLower();
-        while (category_A_answer_3 != "murder")
-        {
-          Console.Write($"That's the wrong answer. Tries left {tries}.");
-          tries--;
-          Console.Write("\nTry again: ");
-          category_A_answer_3 = Console.ReadLine().ToLower();
-          if (category_A_answer_3 != "murder" && tries == 0)
-          {
-            Console.WriteLine("\nOut of tries. Surprisingly, a group of crows is called murder.");
+
+            Console.WriteLine("\nQuestion 2:");
             Task.Delay(1000).Wait();
-            break;
-          }
+            Console.WriteLine("How many wonders of the world are there?");
+            string category_A_answer_2;
+
+            Console.Write("Answer: ");
+            category_A_answer_2 = Console.ReadLine().ToLower();
+            while (category_A_answer_2 != "seven" && category_A_answer_2 != "7")
+            {
+                Console.Write($"That's the wrong answer. Tries left {tries}.");
+                tries--;
+                Console.Write("\nTry again: ");
+                category_A_answer_2 = Console.ReadLine().ToLower();
+                if (category_A_answer_2 != "seven" && category_A_answer_2 != "7" && tries == 0)
+                {
+                    Console.WriteLine("\nOut of tries. The correct answer is seven (7).");
+                    Task.Delay(1000).Wait();
+                    break;
+                }
+            }
+            if (category_A_answer_2 == "seven" || category_A_answer_2 == "7" && tries == 2)
+            {
+                Console.WriteLine("\nCorrect answer");
+                points++;
+            }
+            else if (category_A_answer_2 == "seven" || category_A_answer_2 == "7" && tries != 2)
+            {
+                Console.WriteLine("\nCorrect answer");
+            }
         }
-        if (category_A_answer_3 == "murder")
+
+        if (tries > -1)
         {
-          Console.WriteLine("\nSurprisingly, a group of crows is somehow called murder.");
+
+
+            Console.WriteLine("\nQuestion 3:");
+            Task.Delay(1000).Wait();
+            Console.WriteLine("What is a group of crows called?");
+            string category_A_answer_3;
+
+            Console.Write("Answer: ");
+            category_A_answer_3 = Console.ReadLine().ToLower();
+            while (category_A_answer_3 != "murder")
+            {
+                Console.Write($"That's the wrong answer. Tries left {tries}.");
+                tries--;
+                Console.Write("\nTry again: ");
+                category_A_answer_3 = Console.ReadLine().ToLower();
+                if (category_A_answer_3 != "murder" && tries == 0)
+                {
+                    Console.WriteLine("\nOut of tries. Surprisingly, a group of crows is called murder.");
+                    Task.Delay(1000).Wait();
+                    break;
+                }
+            }
+            if (category_A_answer_3 == "murder" && tries == 2)
+            {
+                Console.WriteLine("\nSurprisingly, a group of crows is somehow called murder.");
+                points++;
+            }
+            else if (category_A_answer_3 == "murder" && tries != 2)
+            {
+                Console.WriteLine("\nSurprisingly, a group of crows is somehow called murder.");
+            }
         }
     }
     if (category == "B")
@@ -177,75 +204,104 @@ while (true)
         category_B_answer_1 = Console.ReadLine().ToLower();
         while (category_B_answer_1 != "64")
         {
-          Console.Write($"That's the wrong answer. Tries left {tries}.");
-          tries--;
-          Console.Write("\nTry again: ");
-          category_B_answer_1 = Console.ReadLine().ToLower();
-          if (category_B_answer_1 != "64" && tries == 0)
-          {
-            Console.WriteLine("\nOut of tries. The correct answer is 64.");
-            Task.Delay(1000).Wait();
-            break;
-          }
+            Console.Write($"That's the wrong answer. Tries left {tries}.");
+            tries--;
+            Console.Write("\nTry again: ");
+            category_B_answer_1 = Console.ReadLine().ToLower();
+            if (category_B_answer_1 != "64" && tries == 0)
+            {
+                Console.WriteLine("\nOut of tries. The correct answer is 64.");
+                Task.Delay(1000).Wait();
+                break;
+            }
         }
-        if (category_B_answer_1 == "64")
+        if (category_B_answer_1 == "64" && tries == 2)
         {
-          Console.WriteLine("\nCorrect answer");
+            Console.WriteLine("\nCorrect answer");
+            points++;
         }
-        Console.WriteLine("\nQuestion 2:");
-        Task.Delay(1000).Wait();
-        Console.WriteLine("Which gaming console has sold the most?");
-        string category_B_answer_2;
+        else if (category_B_answer_1 == "64" && tries != 2)
+        {
+            Console.WriteLine("\nCorrect answer");
+        }
 
-        Console.Write("Answer: ");
-        category_B_answer_2 = Console.ReadLine().ToLower();
-        while (category_B_answer_2 != "ps2" && category_B_answer_2 != "playstation 2")
+        if (tries > -1)
         {
-          Console.Write($"That's the wrong answer. Tries left {tries}.");
-          tries--;
-          Console.Write("\nTry again: ");
-          category_B_answer_2 = Console.ReadLine().ToLower();
-          if (category_B_answer_2 != "ps2" && category_B_answer_2 != "playstation 2" && tries == 0)
-          {
-            Console.WriteLine("\nOut of tries. The correct answer is the Playstation 2.");
-            Task.Delay(1000).Wait();
-            break;
-          }
-        }
-        if (category_B_answer_2 == "playstation 2" || category_B_answer_2 == "ps2")
-        {
-          Console.WriteLine("\nCorrect answer");
-        }
-        Console.WriteLine("\nQuestion 3:");
-        Task.Delay(1000).Wait();
-        Console.WriteLine("What does RNG stand for?");
-        string category_B_answer_3;
 
-        Console.Write("Answer: ");
-        category_B_answer_3 = Console.ReadLine().ToLower();
-        while (category_B_answer_3 != "random number generator")
-        {
-          Console.Write($"That's the wrong answer. Tries left {tries}.");
-          tries--;
-          Console.Write("\nTry again: ");
-          category_B_answer_3 = Console.ReadLine().ToLower();
-          if (category_B_answer_3 != "random number generator" && tries == 0)
-          {
-            Console.WriteLine("\nOut of tries. The correct answer is France.");
+            Console.WriteLine("\nQuestion 2:");
             Task.Delay(1000).Wait();
-            break;
-          }
+            Console.WriteLine("Which gaming console has sold the most?");
+            string category_B_answer_2;
+
+            Console.Write("Answer: ");
+            category_B_answer_2 = Console.ReadLine().ToLower();
+            while (category_B_answer_2 != "ps2" && category_B_answer_2 != "playstation 2")
+            {
+                Console.Write($"That's the wrong answer. Tries left {tries}.");
+                tries--;
+                Console.Write("\nTry again: ");
+                category_B_answer_2 = Console.ReadLine().ToLower();
+                if (category_B_answer_2 != "ps2" && category_B_answer_2 != "playstation 2" && tries == 0)
+                {
+                    Console.WriteLine("\nOut of tries. The correct answer is the Playstation 2.");
+                    Task.Delay(1000).Wait();
+                    break;
+                }
+            }
+            if (category_B_answer_2 == "playstation 2" || category_B_answer_2 == "ps2" && tries == 2)
+            {
+                Console.WriteLine("\nCorrect answer");
+                points++;
+            }
+            else if (category_B_answer_2 == "playstation 2" || category_B_answer_2 == "ps2" && tries != 2)
+            {
+                Console.WriteLine("\nCorrect answer");
+            }
         }
-        if (category_B_answer_3 == "random number generator")
+
+        if (tries > -1)
         {
-          Console.WriteLine("\nCorrect answer");
+
+            Console.WriteLine("\nQuestion 3:");
+            Task.Delay(1000).Wait();
+            Console.WriteLine("What does RNG stand for?");
+            string category_B_answer_3;
+
+            Console.Write("Answer: ");
+            category_B_answer_3 = Console.ReadLine().ToLower();
+            while (category_B_answer_3 != "random number generator")
+            {
+                Console.Write($"That's the wrong answer. Tries left {tries}.");
+                tries--;
+                Console.Write("\nTry again: ");
+                category_B_answer_3 = Console.ReadLine().ToLower();
+                if (category_B_answer_3 != "random number generator" && tries == 0)
+                {
+                    Console.WriteLine("\nOut of tries. The correct answer is France.");
+                    Task.Delay(1000).Wait();
+                    break;
+                }
+            }
+            if (category_B_answer_3 == "random number generator" && tries == 2)
+            {
+                Console.WriteLine("\nCorrect answer");
+                points++;
+            }
+            else if (category_B_answer_3 == "random number generator" && tries != 2)
+            {
+                Console.WriteLine("\nCorrect answer");
+            }
         }
     }
-  Console.WriteLine($"\nCongrats {nameS}, you completed the quiz!");
-  Task.Delay(1500).Wait();
-  Console.WriteLine("Here is a cookie:");
-  Task.Delay(1000).Wait();
-  Console.WriteLine(@"                                                                                
+
+    if (tries > 0 && points == 3)
+    {
+
+        Console.WriteLine($"\nCongrats {nameS}, you aced the quiz!");
+        Task.Delay(1500).Wait();
+        Console.WriteLine("Here is a cookie:");
+        Task.Delay(1000).Wait();
+        Console.WriteLine(@"                                                                                
                                                                                 
                                                                                 
                                                                                 
@@ -277,13 +333,30 @@ while (true)
                                                                                 
                                                                                 
                                                                                 ");
-  string restart = "";
-  Console.WriteLine("\nDo you wish to play again? (Yes/No)");
-  Console.Write(">");
-  restart = Console.ReadLine().ToLower();
-  if (restart == "no" || restart == "n")
-  {
-    Environment.Exit(0);
-  }  
+    }
+    if (tries != 2)
+    {
+
+        Console.WriteLine($"\nCongrats {nameS}, you cleared the quiz!");
+        Task.Delay(1000).Wait();
+    }
+    string restart = "";
+    //Console.WriteLine($"{points}");
+    while (restart != "yes" && restart != "y" && restart != "no" && restart != "n")
+    {
+        Console.WriteLine("\nDo you wish to play again? (Yes/No)");
+        Console.Write(">");
+        restart = Console.ReadLine().ToLower();
+        if (restart == "no" || restart == "n")
+        {
+            Environment.Exit(0);
+        }
+        if (restart != "yes" && restart != "y" && restart != "no" && restart != "n")
+        {
+            Console.WriteLine("Struggling with yes and no are we now?");
+            Task.Delay(750).Wait();
+        }
+    }
+    Console.Clear();
 }
 Console.ReadLine();
